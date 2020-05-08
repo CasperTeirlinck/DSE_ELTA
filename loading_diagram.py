bf19019a98e79fe1d778091e3eb6b29618befe45
Matthijs <matthijsvede@gmail.com>
2020-05-08 09:03:39 +0200

    loading diagram fixes

--- loading_diagram.py
+++ loading_diagram.py
@@ -4,7 +4,7 @@ def stallspeed(CLmax, Vs, rho):
     """
     :param CLmax: CLmax of the aircraft, scalar
     :param Vs: Stall speed, scalar
-    :param rho: Airdensity, scalar
+    :param rho: Airdensity, scalar; sea level can be assumed
     :return: Stall speed requirement, W/S scalar
     """
     return 0.5*rho*(Vs**2)*CLmax
@@ -13,7 +13,7 @@ def stallspeed(CLmax, Vs, rho):
 def takeoff(k, CLTO, sigma, WS):
     """
     :param k: take-off parameter, scalar
-    :param CLTO: take-off CL, scalar
+    :param CLTO: take-off CL, = CLmax,TO/1.1^2 scalar
     :param sigma: density ratio rho/rho0, scalar
     :param WS: plot x values, array
     :return: WP: plot y values, array
@@ -34,7 +34,7 @@ def landing(CLmax, rho, sland, f):
 
 def cruisspeed(etap, rho, rho0, CD0, V, A, e, WS):
     """
-    :param etap: NO IDEA WHAT THIS IS, scalar
+    :param etap: propeller efficiency, scalar
     :param rho: density, scalar
     :param rho0: density at sealvl, scalar
     :param CD0: drag constant, scalar
@@ -44,12 +44,12 @@ def cruisspeed(etap, rho, rho0, CD0, V, A, e, WS):
     :param WS: plot x values, array
     :return: WP: plot y values, array
     """
-    return (0.9/0.8)*etap*(rho/rho0)**(3/4)*((CD0*0.5*rho*V**3)/(0.8*WS) + (0.8*WS)*(1/(np.pi*A*e*0.5*rho*V)))
+    return (0.9/0.8)*etap*(rho/rho0)**(3/4)*((((CD0*0.5*rho*V**3)/(0.8*WS)) + (0.8*WS)*(1/(np.pi*A*e*0.5*rho*V)))**(-1))
 
 
 def climbrate(etap, rho, A, e, CD0, c, WS):
     """
-    :param etap: NO IDEA WHAT THIS IS, scalar
+    :param etap: propeller efficiency, scalar
     :param rho: density, scalar
     :param A: Aspect ratio, scalar
     :param e: oswald efficiency factor
@@ -58,12 +58,12 @@ def climbrate(etap, rho, A, e, CD0, c, WS):
     :param WS: plot x values, array
     :return: plot y values, array
     """
-    return etap/(c+ (np.sqrt(WS*(2/rho)))/(1.345*((A*e)**(3/4))/(CD0**(1/4))))
+    return etap/(c + (np.sqrt(WS*(2/rho)))/(1.345*((A*e)**(3/4))/(CD0**(1/4))))
 
 
 def climbgradient(etap, cV, CD, CL, rho, WS):
     """
-    :param etap: NO IDEA WHAT THIS IS, scalar
+    :param etap: propeller efficiency, scalar
     :param cV: NO IDEA WHAT THIS IS, scalar
     :param CD: drag coefficient, scalar
     :param CL: lift coefficient, scalar
@@ -71,4 +71,4 @@ def climbgradient(etap, cV, CD, CL, rho, WS):
     :param WS: plot x values, array
     :return: plot y values, array
     """
-    return etap/(np.sqrt(WS)*(cV + CD/CL)*np.sqrt((2/rho)*(1/CL))
+    return etap/(np.sqrt(WS)*(cV + CD/CL)*np.sqrt((2/rho)*(1/CL)))
