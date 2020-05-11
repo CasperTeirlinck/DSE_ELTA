585c090edf68005f35fb678f41414bba005bd1e6
MaxMeijkamp <maximilian.meijkamp@gmail.com>
2020-05-11 14:24:09 +0200

    Added TO and climb contribution

--- propulsion.py
+++ propulsion.py
@@ -5,31 +5,34 @@ range_m = 250000
 endurance_s = 9000
 g0 = 9.80665 # m/s2
 rho0 = 1.225 # kg/m3
+hcr = 914.4 # m
+Vy = 2 # m/s
 taxitime = 330 # s
-taxipower = 0.05 #
+taxipower = 0.05 # -
+climbtime = hcr/Vy + 30 # s, added time for take-off
+climbpower = 1 # -
+# Descent assumed to be included in endurance and range
+
 
 def range_battery(CD0, e, A, WTO, Especific, eff_total, powerloading):
     # Returns the battery weight in N. WTO required to be in N.
-    # TODO: Add climb and descent battery mass
     CL = sqrt(pi * A * e * CD0)
     CD = 2 * CD0
-    LoverD = CL/CD
-    # battery_weight_fraction = range_m / LoverD * g0 / Especific / eff_total
-    total_energy_range = WTO * range_m / (CL/CD)
-    total_energy_range += taxipower * powerloading * WTO * taxitime
-    return total_energy_range / (Especific * eff_total) * g0
+    total_energy_range = range_m / (CL/CD)
+    total_energy_range += taxipower / powerloading * taxitime * 2 # Taxi before TO and after L
+    total_energy_range += climbpower / powerloading * climbtime
+    return total_energy_range / (Especific * eff_total) * WTO * g0
 
 
 def endurance_battery(CD0, e, A, WTO, Especific, eff_total, powerloading, wingloading):
     # Returns the battery weight in N. WTO required to be in N.
-    # TODO: Add climb and descent battery mass
-    S = WTO / wingloading
     CD = 4 * CD0
     CL = sqrt(3 * pi * CD0 * e * A)
-    V_loiter = sqrt(WTO / (0.5 * rho0 * S * CL))
-    total_energy_loiter = endurance_s * 0.5 * rho0 * V_loiter**3 * S * CD
-    total_energy_loiter += taxipower * powerloading * WTO * taxitime
-    return total_energy_loiter / (Especific * eff_total) * g0
+    V_loiter = CL/CD / sqrt(0.5 * rho0 * CL**3 / CD**2 / wingloading)
+    total_energy_loiter = endurance_s * 0.5 * rho0 * V_loiter**3 * CL * CD / wingloading
+    total_energy_loiter += taxipower / powerloading * taxitime * 2 # Taxi before TO and after L
+    total_energy_loiter += climbpower / powerloading * climbtime
+    return total_energy_loiter / (Especific * eff_total) * WTO * g0
 
 
 
