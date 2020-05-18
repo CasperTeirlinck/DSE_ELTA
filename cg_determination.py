5ae1970abf7931a9e0b0b37dc37f50f867eca1ef
eoverbosch <eliasoverbosch@hotmail.com>
2020-05-18 10:14:48 +0200

    Added empennage contribution to CG

--- cg_determination.py
+++ cg_determination.py
@@ -1,6 +1,6 @@
 from variables import *
 
-
+## AIRCRAFT GROUP CONTRIBUTIONS
 # Calculate x-coordinate of the fuselage group CG
 def X_fuselagegroupCG(variables):
     if variables.wing_mounted_engine:
@@ -34,21 +34,39 @@ def winggroup_weight(variables):
 def wing_position(variables):
     return X_fuselagegroupCG(variables) + variables.MAC * (chordwise_winggroupCG(variables) * winggroup_weight(variables)/fuselagegroup_weight(variables) - variables.chordwise_cg_OEW * (1 + winggroup_weight(variables) / fuselagegroup_weight(variables)))
 
-
-# Calculate x-coordinate of total aircraft CG OEW
-def x_OEWCG(variables):
+## TAILLESS AIRCRAFT CG
+# Calculate x-coordinate of tailless aircraft CG OEW
+def x_OEWCG_tailless(variables):
     return (winggroup_weight(variables)*wing_position(variables) + fuselagegroup_weight(variables)*X_fuselagegroupCG(variables)) / (winggroup_weight(variables) + fuselagegroup_weight(variables))
 
+# Calculate most forward CG of tailless aircraft
+def x_forwardCG_tailless(variables):
+    return min((variables.WPL*variables.x_cg_passenger + (winggroup_weight(variables) + fuselagegroup_weight(variables)) * x_OEWCG_tailless(variables))/ (winggroup_weight(variables) + fuselagegroup_weight(variables) + variables.WPL), x_OEWCG_tailless(variables))
 
+# Calculate most aft CG of tailless aircraft
+def x_aftCG_tailless(variables):
+    return max((variables.WPL*variables.x_cg_passenger + (winggroup_weight(variables) + fuselagegroup_weight(variables)) * x_OEWCG_tailless(variables)) / (winggroup_weight(variables) + fuselagegroup_weight(variables) + variables.WPL), x_OEWCG_tailless(variables))
 
-def x_forwardCG(variables):
-    return min((variables.WPL*variables.x_cg_passenger + (winggroup_weight(variables) + fuselagegroup_weight(variables)) * x_OEWCG(variables))/ (winggroup_weight(variables) + fuselagegroup_weight(variables) + variables.WPL), x_OEWCG)
-
-def x_aftCG(variables):
-    return max((variables.WPL*variables.x_cg_passenger + (winggroup_weight(variables) + fuselagegroup_weight(variables)) * x_OEWCG(variables)) / (winggroup_weight(variables) + fuselagegroup_weight(variables) + variables.WPL), x_OEWCG)
-
-
-def cg_calculations(variables):
-    variables.xcg_frw = x_forwardCG(variables)
-    variables.xcg_aft = x_aftCG(variables)
+def cg_calculations_tailless(variables):
+    variables.xcg_frw = x_forwardCG_tailless(variables)
+    variables.xcg_aft = x_aftCG_tailless(variables)
     return variables
+
+## TOTAL AIRCRAFT CG
+# Calculate x-coordinate of total aircraft CG OEW
+def x_OEW_total(variables):
+    W_htail = Empennage_Estimation(variables)[0]
+    return (winggroup_weight(variables)*wing_position(variables) + fuselagegroup_weight(variables)*X_fuselagegroupCG(variables) + W_htail*variables.x_htail) / (winggroup_weight(variables) + fuselagegroup_weight(variables) + W_htail)
+
+# Calculate most forward CG of total aircraft
+def x_forwardCG_total(variables):
+    return min((variables.WPL*variables.x_cg_passenger + (winggroup_weight(variables) + fuselagegroup_weight(variables)) * x_OEWCG_total(variables)) / (winggroup_weight(variables) + fuselagegroup_weight(variables) + variables.WPL), x_OEWCG_total(variables))
+
+# Calculate most aft CG of total aircraft
+def x_aftCG_total(variables):
+    return max((variables.WPL*variables.x_cg_passenger + (winggroup_weight(variables) + fuselagegroup_weight(variables)) * x_OEWCG_total(variables)) / (winggroup_weight(variables) + fuselagegroup_weight(variables) + variables.WPL), x_OEWCG_total(variables))
+
+def cg_calculations_total(variables):
+    variables.xcg_frw = x_forwardCG_total(variables)
+    variables.xcg_aft = x_aftCG_total(variables)
+    return variables    
