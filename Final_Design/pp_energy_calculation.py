ebda3d94c6adbcb9574f931fb0c8612be9ddda80
Matthijs van Ede <51402269+MatthijsEde@users.noreply.github.com>
2020-06-02 17:40:45 +0200

    added testcases to pp_energy_calculation.py

--- Final_Design/pp_energy_calculation.py
+++ Final_Design/pp_energy_calculation.py
@@ -105,13 +105,12 @@ def battery_mass_total(variables):
     WTO_range = variables.WTO_range
 
     # calculation of the battery mass fractions
-    bms_endurance = bms_engine(variables, taxi = 2, climb = 1, endurance_s = 9000.0, range_m = 0.0)
-    bms_range = bms_engine(variables, taxi = 2, climb = 1, endurance_s = 0.0, range_m = 250000.0)
+    bms_endurance = bms_engine(variables, taxi = 2, climb = 1, endurance_s = variables.endurance_s, range_m = 0.0)
+    bms_range = bms_engine(variables, taxi = 2, climb = 1, endurance_s = 0.0, range_m = variables.range_m)
 
     # caculation of the battery weights in newtons
     W_bat_endurance = bms_endurance * WTO_endurance
     W_bat_range = bms_range * WTO_range
-
 
     # total battery weight in newtons
     W_bat = max(W_bat_endurance, W_bat_range) + (energy_avionics()/variables.Especif_bat)
@@ -139,18 +138,66 @@ class Test_variables_pp:
         self.g0             = 9.80665
         self.WTO_endurance  = 9978.38
         self.WTO_range      = self.WTO_endurance - (100*self.g0)
+        self.range_m        = 250000
+        self.endurance_s    = 2.5*3600
+
+class Test_alpha_electro:
+    def __init__(self):
+        self.CD0            = 0.0280
+        self.A              = 10.5**2/9.51
+        self.e              = 0.83
+        self.rho0           = 1.225
+        self.WP             = (550*9.80665)/(60*1000)
+        self.WS             = (550*9.80665)/(9.51)
+        self.Especif_bat    = 900000
+        self.rho_bat        = 500*3600
+        self.eff_tot_prop   = 0.72
+        self.g0             = 9.80665
+        self.WTO_endurance  = 550*9.80665
+        self.WTO_range      = 0
+        self.range_m        = 0
+        self.endurance_s    = 3600 + (20*60)
+
+
+class Test_eflyer2:
+    def __init__(self):
+        self.CD0            = 0.0280
+        self.A              = 12**2/12.0
+        self.e              = 0.83
+        self.rho0           = 1.225
+        self.WP             = (862*9.80665)/(90*1000)
+        self.WS             = (862*9.80665)/12.0
+        self.Especif_bat    = 900000
+        self.rho_bat        = 500*3600
+        self.eff_tot_prop   = 0.72
+        self.g0             = 9.80665
+        self.WTO_endurance  = 862*9.80665
+        self.WTO_range      = 0
+        self.range_m        = 0
+        self.endurance_s    = 3.5*3600
+
 
 if __name__ ==  "__main__":
     test_v = Test_variables_pp()
     W, V = battery_mass_total(test_v)
 
-    print("------------------------------------------")
+    print("Our aircraft:\n------------------------------------------")
     print("Battery weight [N]   =", W)
     print("Battery weight [kg]  =", W/test_v.g0)
     print("Battery volume [L]   =", V)
     print("Battery volume [m^3] =", V*0.001)
     print("------------------------------------------")
 
+    test_v_2 = Test_alpha_electro()
+    W2, V2 = battery_mass_total(test_v_2)
+
+    print("\nAlpha electro:\n------------------------------------------")
+    print("DATA: batt mass [kg] = 114")
+    print("Battery weight [N]   =", W2)
+    print("Battery weight [kg]  =", W2/test_v_2.g0)
+    print("Battery volume [L]   =", V2)
+    print("Battery volume [m^3] =", V2*0.001)
+    print("------------------------------------------")
 
 
 
