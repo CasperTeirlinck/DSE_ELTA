199926d0aae1f1fc19ae6da7bffbd1884c421570
Casper Teirlinck <casperteirlinck@gmail.com>
2020-06-05 13:49:39 +0200

    liftdistribution

--- Final_Design/aerodynamics/LiftDistribution.py
+++ Final_Design/aerodynamics/LiftDistribution.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 
+import variables_aero as v
+
 
 def TransformSpan(y,wingspan):
     return -np.arccos(2*y/wingspan)
@@ -30,5 +32,28 @@ def CalculateLiftSlope(theta,wingspan,a_airfoil1,a_airfoil2):
     return 2*np.pi
 
 
-def CalculateDistributionCoefficients(wingspan,sweep,liftslope_1,liftslope2,zeroliftangle_1,zeroliftangle_2,Fuselage=False,N):
+def CalculateDistributionCoefficients(wingspan,sweep,liftslope_1,liftslope2,zeroliftangle_1,zeroliftangle_2,N,Fuselage=False):
     b = wingspan
+
+
+
+
+
+
+
+alpha = np.radians(5)
+V = 50 # [m/s]
+
+Acoeff = np.array([
+    [0.2316, 0], # A_n, aL=0 _n
+    [0.0277, 0], 
+    [0.0040, 0]
+])
+A = [ A[0]*alpha + A[1] for A in Acoeff ]
+
+CL = np.pi * v.A * A[0]
+
+def circ(theha):
+    for n in range(1, )
+    return 2 * v.b * V * np.sum(A * np.sin(n*theta))
+
