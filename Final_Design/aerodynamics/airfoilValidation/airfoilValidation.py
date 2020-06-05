560b67a4d7d08f954d657e5229baa5e0453826ae
Casper Teirlinck <casperteirlinck@gmail.com>
2020-06-05 10:15:46 +0200

    airfoilvalidation

--- Final_Design/aerodynamics/airfoilValidation/airfoilValidation.py
+++ Final_Design/aerodynamics/airfoilValidation/airfoilValidation.py
@@ -50,16 +50,20 @@ def get_data_naca(foil, Re):
         if Re == 6:
             Cl = [-0.85, -0.9, -0.85, -0.65,  -0.4,  -0.2,      0,    0.2,   0.45,    0.6,   0.85,  1.05,   1.2,   1.4,  1.51, 1.55, 1.49]
             Cd = [    0,    0, 0.0105, 0.0095, 0.008, 0.0075, 0.007, 0.0068, 0.0065, 0.0067, 0.0072, 0.0082, 0.0097, 0.014, 0.017, 0.018,    0]
-    if foil == '0009': # pdf p 463
-        a = []
+    if foil == '652415': # pdf p 463
+        a = [-16, -14, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
         if Re == 3:
-            Cl = []
-            Cd = []    
+            Cl = [-0.6, -1.0, -0.95,    -0.8,   -0.6,   -0.4,  -0.18,   0.05,    0.3,    0.5,   0.75,   0.9,  1.05,  1.15, 1.25, 1.34, 1.38, 1.37, 1.3]
+            Cd = [   0,    0,     0,  0.0134, 0.0105,  0.009, 0.0078, 0.0075,  0.005, 0.0056, 0.0057, 0.0105, 0.016, 0.024,    0,    0,    0,    0,  0]
+        if Re == 6:
+            Cl = [-0.6, -1.0, -0.95,    -0.8,   -0.6,   -0.4,  -0.18,   0.05,    0.3,    0.5,   0.75,   0.9,   1.1,  1.25,  1.35, 1.43, 1.5, 1.53, 1.53]
+            Cd = [   0,    0,     0,   0.011, 0.0092, 0.0079,  0.007, 0.0067, 0.0044, 0.0045, 0.0048, 0.010, 0.013, 0.018, 0.028,   0,    0,    0,    0]
 
     return a, Cl, Cd
 
 """ Data Setup """
-foil = '4415'
+# foil = '4415' # why?: midterm choice
+foil = '652415' # why?: 6-series: good lift and drag (ref midterm), potential choice in airfoil selection
 Re = 6
 
 a, Cl, Cd = get_data_xfoil(foil, Re)
@@ -71,16 +75,28 @@ Cla_y = Cl
 naca_Cla_x = naca_a
 naca_Cla_y = naca_Cl
 # Cl Cd
-if Re == 3:
-    ClCd_x = Cl[2:-2]
-    ClCd_y = Cd[2:-2]
-    naca_ClCd_x = naca_Cl[2:-2]
-    naca_ClCd_y = naca_Cd[2:-2]
-if Re == 6:
-    ClCd_x = Cl[2:-1]
-    ClCd_y = Cd[2:-1]
-    naca_ClCd_x = naca_Cl[2:-1]
-    naca_ClCd_y = naca_Cd[2:-1]
+if foil == '4415':
+    if Re == 3:
+        ClCd_x = Cl[:]
+        ClCd_y = Cd[:]
+        naca_ClCd_x = naca_Cl[2:-2]
+        naca_ClCd_y = naca_Cd[2:-2]
+    if Re == 6:
+        ClCd_x = Cl[2:-1]
+        ClCd_y = Cd[2:-1]
+        naca_ClCd_x = naca_Cl[2:-1]
+        naca_ClCd_y = naca_Cd[2:-1]
+if foil == '652415':
+    if Re == 3:
+        ClCd_x = Cl[:]
+        ClCd_y = Cd[:]
+        naca_ClCd_x = naca_Cl[3:-5]
+        naca_ClCd_y = naca_Cd[3:-5]
+    if Re == 6:
+        ClCd_x = Cl[:]
+        ClCd_y = Cd[:]
+        naca_ClCd_x = naca_Cl[3:-4]
+        naca_ClCd_y = naca_Cd[3:-4]
 
 """ Errors """
 def calc_err(x1, y1, x2, y2, errrange, rangestep):
