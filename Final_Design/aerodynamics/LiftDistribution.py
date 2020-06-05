c668ce0f1f865974d0638ad00da1491db54e639e
eoverbosch <eliasoverbosch@hotmail.com>
2020-06-05 11:47:33 +0200

    .

--- Final_Design/aerodynamics/LiftDistribution.py
+++ Final_Design/aerodynamics/LiftDistribution.py
@@ -9,6 +9,7 @@ def TransformSpan(y,wingspan):
 def TransformTheta(theta,wingspan):
     return -.5*wingspan*np.cos(theta)
 
+
 def CalculateChord(theta,taper,S,span):
     y = TransformTheta(theta,wingspan)
     return 2*S/(wingspan+taper*wingspan)*(1-(1/wingspan-taper/wingspan)*abs(2*y))
@@ -24,9 +25,10 @@ def CalculateTwistAngle(theta,wingspan,twist,gamma):
         print("NOT IMPLEMENTED YET")
         return None
 
+
 def CalculateLiftSlope(theta,wingspan,a_airfoil1,a_airfoil2):
     return 2*np.pi
 
+
 def CalculateDistributionCoefficients(wingspan,sweep,liftslope_1,liftslope2,zeroliftangle_1,zeroliftangle_2,Fuselage=False,N):
     b = wingspan
-    
