a3ceea31bf424c6d54c520bafe8cf5d9a9575e07
eoverbosch <eliasoverbosch@hotmail.com>
2020-06-09 16:54:26 +0200

    Updated airfoil parameters

--- Final_Design/aerodynamics/variables_aero.py
+++ Final_Design/aerodynamics/variables_aero.py
@@ -10,9 +10,6 @@ gamma = 0
 # Clmax_r = 1.6 # Clmax root airfoil
 # Clmax_t = 1.5 # Clmax tip airfoil
 
-Clmax_t = 1.6 # Clmax root airfoil
-Clmax_r = 1.5 # Clmax tip airfoil
-
 # Cla_r = 0.7/np.radians(7)
 # Cla_t = 1/np.radians(10)
 # Cd0_r = 0.006
@@ -20,9 +17,26 @@ Clmax_r = 1.5 # Clmax tip airfoil
 # a0_r = np.radians(-2)
 # a0_t = np.radians(-4)
 
+
+Clmax_t = 1.6 # Clmax root airfoil
+Clmax_r = 1.5 # Clmax tip airfoil
+
+Cla_t = 6.016
+Cla_r = 1/np.radians(10)
+Cd0_t = 0.004
+Cd0_r = 0.004
+a0_t = np.radians(-3)
+a0_r = np.radians(-4)
+
+"""
+VALIDATION DATA
+Clmax_t = 1.6 # Clmax root airfoil
+Clmax_r = 1.5 # Clmax tip airfoil
+
 Cla_t = 0.7/np.radians(7)
 Cla_r = 1/np.radians(10)
 Cd0_t = 0.006
 Cd0_r = 0.004
 a0_t = np.radians(-2)
 a0_r = np.radians(-4)
+"""
