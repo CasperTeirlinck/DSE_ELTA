01c7fe86909cf6cfa6006a03c2e261b218dbb70f
Casper Teirlinck <casperteirlinck@gmail.com>
2020-06-10 09:01:11 +0200

    ...

--- Final_Design/aerodynamics/variables_aero.py
+++ Final_Design/aerodynamics/variables_aero.py
@@ -7,16 +7,6 @@ V = 50 # [m/s]
 taper = 0.9 # [-] Taper ratio
 twist = np.radians(2)
 gamma = 0
-# Clmax_r = 1.6 # Clmax root airfoil
-# Clmax_t = 1.5 # Clmax tip airfoil
-
-# Cla_r = 0.7/np.radians(7)
-# Cla_t = 1/np.radians(10)
-# Cd0_r = 0.006
-# Cd0_t = 0.004
-# a0_r = np.radians(-2)
-# a0_t = np.radians(-4)
-
 
 Clmax_t = 1.6 # Clmax root airfoil
 Clmax_r = 1.5 # Clmax tip airfoil
