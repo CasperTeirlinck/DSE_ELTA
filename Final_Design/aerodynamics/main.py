d28073e57cfac76b39a035b16773e99668113de3
eoverbosch <eliasoverbosch@hotmail.com>
2020-06-11 10:00:54 +0200

    Added optimisation loop to main

--- Final_Design/aerodynamics/main.py
+++ Final_Design/aerodynamics/main.py
@@ -30,30 +30,41 @@ def plotLiftDistribution(y, Cl_range, ClmaxDistr=None, legend=False):
     plt.show()
 
 if __name__ == "__main__":
-
+    # OPTIMISATION LOOP
     WingList = np.array([])
     for taper in np.linspace(0,1.0,11):
         for twist in linspace(0,6,7):
+            if twist = 0:
+                print(taper*100,"% completed")
 
             wing = WingPlanform(v.S, v.A, taper, twist, v.gamma)
             wing.setAirfoils(v.Clmax_r, v.Clmax_t, v.Cla_r, v.Cla_t, v.a0_r, v.a0_t, v.Cd0_r, v.Cd0_t, v.deltaAlphaStall_r, v.deltaAlphaStall_t)
     # wing.calcCoefficients(1000, tipCutoff=0.5) # use to get sensible CDi
-            wing.calcCoefficients(100, tipCutoff=0.9)
+            wing.calcCoefficients(100, tipCutoff=0.6)
+            
 
-            CLmax, alphaMax, Cl_distrMax, yPntsMax
+            CLmax, alphaMax, Cl_distrMax, yPntsMax, ClmaxDistr, stallpos = wing.calcCLmax()
+            CDi1,e1 = wing.calcCDi(np.radians(2))
+            CDi2,e2 = wing.calcCDi(np.radians(6))
+            CDi3,e3 = wing.calcCDi(np.radians(10))
+            if stallpos <= 0.5*wing.wingspan and CLmax >= 1.40:
+                Winglist = np.append(WingList,np.array([taper,twist,CLmax,stallpos,alphaMax,e1,e2,e3]))
+
+
+
     # alpha = np.radians(5)
     # Cl_distr, yPnts = wing.calcLiftDistribution(alpha, 100)
     # plotLiftDistribution(yPnts, [Cl_distr])
 
     """ CLa """
-    if False:
-        CLa = wing.calcCLa()
-        print(f'CLa = {CLa} [/rad] or {CLa*np.pi/180} [deg]')
+    # if False:
+    #     CLa = wing.calcCLa()
+    #     print(f'CLa = {CLa} [/rad] or {CLa*np.pi/180} [deg]')
 
-    """ CLmax """
-    if True:
-        CLmax, alphaMax, Cl_distrMax, yPntsMax, ClmaxDistr = wing.calcCLmax(plotProgression=False, printMaxLoc=True)
-        print(f'CLmax = {round(CLmax, 2)} @ a = {round(np.degrees(alphaMax), 2)}')
-        plotLiftDistribution(yPntsMax, [Cl_distrMax], ClmaxDistr=ClmaxDistr, legend=True)
+    # """ CLmax """
+    # if False:
+    #     CLmax, alphaMax, Cl_distrMax, yPntsMax, ClmaxDistr, stallpos = wing.calcCLmax(plotProgression=False, printMaxLoc=True)
+    #     print(f'CLmax = {round(CLmax, 2)} @ a = {round(np.degrees(alphaMax), 2)}')
+    #     plotLiftDistribution(yPntsMax, [Cl_distrMax], ClmaxDistr=ClmaxDistr, legend=True)
 
     
