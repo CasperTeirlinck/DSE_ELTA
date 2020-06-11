c2c87340dd5f70191fdf49e04623992148724e84
Casper Teirlinck <casperteirlinck@gmail.com>
2020-06-11 15:46:25 +0200

    aero design plots

--- Final_Design/aerodynamics/main.py
+++ Final_Design/aerodynamics/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import variables as v
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
+from matplotlib.lines import Line2D
+from collections import OrderedDict
 import os
 
 from WingPlanform import WingPlanform
@@ -43,7 +45,8 @@ def optimize():
             CLmax, alphaMax, Cl_distrMax, yPntsMax, ClmaxDistr, stallpos = wing.calcCLmax()
             espan = wing.calcespan()
 
-            if stallpos <= 0.5*wing.b and CLmax >= 1.40:
+            # if stallpos <= 0.5*wing.b and CLmax >= 1.40:
+            if True:
                 WingList.append([taper, twist, CLmax, stallpos, alphaMax, espan])
 
     with open('Final_Design/aerodynamics/winglist.csv', 'w') as file:
@@ -53,48 +56,81 @@ def optimize():
                 file.write(f'{round(prop, 3)}, ')
             file.write('\n')
 
+
 def readWinglist():
     taper_lst = []
     twist_lst = []
     CLmax_lst = []
     espan_lst = []
     
-    # CLmax_taper = {}
     with open('Final_Design/aerodynamics/winglist.csv', 'r') as f:
         for line in f.readlines()[1:]:
             columns = line.strip().split(',')
             if len(columns) > 0:
-                # CLmax_taper.[columns[1]] = np.append(CLmax_taper.[columns[1]], )
                 taper_lst.append(float(columns[0]))
                 twist_lst.append(float(columns[1]))
                 CLmax_lst.append(float(columns[2]))
+                espan_lst.append(float(columns[5]))
 
-    # CLmax_taper = {}
+    idx = np.argmax(espan_lst)
+    bestWing = [taper_lst[idx], twist_lst[idx], CLmax_lst[idx], espan_lst[idx]]
 
-    return taper_lst, twist_lst, CLmax_lst, espan_lst
+    taper = []
+    for i, twist in enumerate(twist_lst):
+        if twist == twist_lst[0]:
+            taper.append(taper_lst[i])
 
-def plotDesignParams(x, y, z, xlabel='', ylabel='', zlabel=''):
+    CLmax = {}
+    for i, twist in enumerate(twist_lst):
+        if not twist in CLmax.keys(): CLmax[twist] = []
+        CLmax[twist].append(CLmax_lst[i])
+    
+    espan = {}
+    for i, twist in enumerate(twist_lst):
+        if not twist in espan.keys(): espan[twist] = []
+        espan[twist].append(espan_lst[i])
+
+    return bestWing, taper, OrderedDict(CLmax), OrderedDict(espan)
+
+def plotDesignParams(x, y1, y2, xlabel='', y1label='', y2label=''):
     fig = plt.figure(figsize=(10, 4.5))
-    ax1 = fig.add_subplot(111)
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
 
-    ax1.plot(x, CLmax_lst, linewidth=2, color='blue', marker='o', fillstyle='none', markevery=2)
+    colors = plt.cm.viridis(np.linspace(0, 1, len(y1)))
+
+    for i, twist in enumerate(y1):
+        ax1.plot(x, y1[twist], linewidth=2, color=colors[i])
+    for i, twist in enumerate(y2):
+        ax2.plot(x, y2[twist], linewidth=2, color=colors[i])
 
     ax1.axvline(x=0, linewidth=2, color='black')
-    ax1.axhline(y=0, linewidth=2, color='black')
+    ax2.axvline(x=0, linewidth=2, color='black')
     ax1.set_xlabel(xlabel)
-    ax1.set_ylabel(ylabel)
+    ax1.set_ylabel(y1label)
+    ax2.set_xlabel(xlabel)
+    ax2.set_ylabel(y2label)
     ax1.xaxis.grid(color='black', linestyle='--')
     ax1.yaxis.grid(color='black', linestyle='--')
-    plt.legend(loc='upper right')
+    ax2.xaxis.grid(color='black', linestyle='--')
+    ax2.yaxis.grid(color='black', linestyle='--')
+    legend = [Line2D([0], [0], color=colors[0], lw=4), Line2D([0], [0], color=colors[int(len(colors)/2)], lw=4), Line2D([0], [0], color=colors[-1], lw=4)]
+    plt.legend(legend, [
+        f'twist={round(np.degrees(sorted(y1.keys())[0]),0)} deg',
+        f'twist={round(np.degrees(sorted(y1.keys())[int(len(y1)/2)]),0)} deg',
+        f'twist={round(np.degrees(sorted(y1.keys())[-1]),0)} deg'
+    ], loc='lower center')
     fig.suptitle('', fontsize=16, y=0.97)
     plt.tight_layout(rect=[0, 0, 1, 0.93])
     plt.show()
 
 if __name__ == "__main__":
     
-    optimize()
-    # readWinglist()
-    # plotDesignParams(taper_lst, CLmax_lst, twist_lst, 'Taper', 'CLmax', 'Twist [deg]')
+    # optimize()
+    bestWing, taper, CLmax, espan = readWinglist()
+    print(f'\nOptimized wing:\t taper={bestWing[0]} \t twist={bestWing[1]} \t CLmax={bestWing[2]} \t espan={bestWing[3]} \n')
+    
+    plotDesignParams(taper, espan, CLmax, 'Taper', 'e span', 'CLmax')
 
 
     # wing = WingPlanform(v.S, v.A, taper, twist, v.gamma)
