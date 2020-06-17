59ad9569eca36b2180f78397ba91f2f731b7740b
Matthijs <matthijsvede@gmail.com>
2020-06-17 13:47:24 +0200

    making battery_main.py ready for integration#2

--- Final_Design/Power_and_Propulsion/battery_main.py
+++ Final_Design/Power_and_Propulsion/battery_main.py
@@ -15,10 +15,6 @@ class Variables_battery:
         self.A = 10.1
         self.e = 0.85
         self.rho0 = 1.225
-        self.WP = 0.122
-        self.WS = 508
-        self.Especif_bat = 900000
-        self.rho_bat = 500 * 3600
         self.eff_tot_prop = 0.95*0.88
         self.eff_batt = 0.95
         self.g0 = 9.80665
@@ -26,8 +22,8 @@ class Variables_battery:
         self.WTO_range = self.WTO_endurance  # THIS IS OFCOURSE WRONG BUT I DON'T HAVE A BETTER VALUE FOR NOW
         self.range_m = 250000
         self.endurance_s = 2.5 * 3600
-        self.P_max = self.WTO_endurance / self.WP
-        self.S = self.WTO_endurance / self.WS
+        self.P_max = 65*1000 #[W]
+        self.S = 15.6
 
         # For the battery composition
         self.V_req_bat = 400    # Voltage required for engine subsytem [V]
@@ -36,8 +32,8 @@ class Variables_battery:
 
 
 def main_bat(variables):
-    E_engine_range = ppe.energy_engine(variables, endurance_s = 0.0, range_m = 250000.0) # Energy for engine in [J] for range req.
-    E_engine_endur = ppe.energy_engine(variables, endurance_s = 9000.0, range_m = 0.0) # Energy for engine in [J] for endurance req.
+    E_engine_range = ppe.energy_engine(variables, endurance_s = 0.0, range_m = variables.range_m) # Energy for engine in [J] for range req.
+    E_engine_endur = ppe.energy_engine(variables, endurance_s = variables.endurance_s, range_m = 0.0) # Energy for engine in [J] for endurance req.
     E_engine = max(E_engine_range, E_engine_endur) # Energy for engine in [J]
     E_avionics = ppe.energy_avionics(variables) # Energy for avionics in [J]
 
@@ -49,7 +45,6 @@ def main_bat(variables):
     E_total = E_engine + E_avionics
 
     # Sizing the battery
-    # Bat = main battery
     # Different cell types
     cell1 = "Sam_21700_50E"
     cell2 = "Sam_21700_40T"
@@ -62,16 +57,17 @@ def main_bat(variables):
     bat = batc.num_of_cells(cell_data, E_req = E_total/3600, V_req = variables.V_req_bat, I_req = variables.I_req_bat, DoD = variables.DOD)
 
     # Calculating mass and volume based on cells only
-    m_bat = bat[2] * cell_data.mass * 1.15
-    v_bat = bat[2] * cell_data.volume * 1000 # from m^3 to L
+    m_bat_cells = bat[2] * cell_data.mass
+    v_bat_cells = bat[2] * cell_data.volume * 1000 # from m^3 to L
 
+    # Actual mass
+    m_bat = m_bat_cells * 1.15
 
     # Calculating the actual energy produced due to rounding in the cell composition
     E_bat_produced = ((variables.DOD/100)*bat[1] * cell_data.C_Ah * bat[0]*cell_data.V_nom)/1000
 
-
     print("Mass bat [kg]:", m_bat)
-    print("Cells volume [L]:", v_bat)
+    print("Cells volume [L]:", v_bat_cells)
     print("E required kWh:", E_total/(3600*1000))
     print("E produced kWh:", E_bat_produced)
     return None
